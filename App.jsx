7ab49e05b5a1d522aff8c1d75a69d09b1213fb0f
NaseeraAmeer <121430943+NaseeraAmeer@users.noreply.github.com>
2023-08-29 20:21:26 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,72 +1,181 @@
-import { useState } from "react";
+import { useState, useSyncExternalStore } from "react";
 import axios from "axios";
 
 function App() {
 
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-  const[isloggedIn,setIsLoggedIn]=useState(false);
-  const[user,setUser]=useState(null);
-  const[errorMsg,setErrorMsg]=useState("");
-  const handleEmail=(e)=>{
-  e.preventDefault();
-  setEmail(e.target.value)}
+  const [pageType, setPageType] = useState("login")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isloggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [firtName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [signupEmail, setSignupEmail] = useState("")
+  const [signupPassword, setSignupPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [samePassword, setSamePassword] = useState(false)
 
-  const handlePassword=(e)=>{
-  e.preventDefault();
-  setPassword(e.target.value)
- }
- 
+
+
+
+
+
+  const handleEmail = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value)
+  }
+
+  const handleFirstName = (e) => {
+    e.preventDefault();
+    setFirstName(e.target.value)
+  }
+
+  const handleLastName = (e) => {
+    e.preventDefault();
+    setLastName(e.target.value)
+  }
+
+  const handleSignupEmail = (e) => {
+    e.preventDefault();
+    setSignupEmail(e.target.value)
+  }
+
+  const handleSignupPassword = (e) => {
+    e.preventDefault();
+    setSignupPassword(e.target.value)
+  }
+
+  const handleConfirmPassword = (e) => {
+    e.preventDefault();
+    setConfirmPassword(e.target.value);
+    if( password ===e.target.value){
+      setSamePassword(true);
+    }else{
+      setSamePassword(false);
+    }
+  }
+
+  const handleSignupSummit = (e) => {
+    e.preventDefault();
+   axios.post("http://localhost:4000/signup",{
+    firtName,
+    lastName,
+    email:signupEmail,
+    password:signupPassword})
+  }
+
+  const handlePassword = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value)
+  }
+
   const submitLogin = (e) => {
     e.preventDefault();
-   
-  
+
+    console.log(email, password);
     axios
-    .post("http://localhost:4000/login", {email, password
-    })
+      .post("http://localhost:4000/login", {
+        email, password
+      })
       .then((result) => {
         console.log(result.data);
-        if(result.data.message=== "Login success!"){
-          setUser(result.data.user)   
+        if (result.data.message === "Login success!") {
+          setUser(result.data.user)
           setIsLoggedIn(true);
-        
+
         }
-        
+
       })
-      .catch(err=>{
+      .catch(err => {
         console.log("error from axios,error");
-          setIsLoggedIn(false);
-          setErrorMsg(err.response.data.message);
+        setIsLoggedIn(false);
+        setErrorMsg(err.response.data.message);
       })
 
   };
-  return(
-  <> 
-    {isloggedIn ? (
+  return (
     <>
-    
-    <h1 style={{color:"blue"}}>Login success! Hello{ user.name}</h1>:
-   <div style={{border:"solid black 2px"}}>
-    <h2>Role:{user.role}</h2>
-    <h4>Employee code:{user.employeeCode}</h4>
-   </div>
-   </>
-    ):(<form>
+      {pageType === "login" ?
 
-      <h1>Login</h1>
-      <div>
-      <label> Username:</label> 
-      <input type="email" onChange={handleEmail}/> 
-      </div>
-      <div>
-      <label > Password:</label> 
-      <input type="password"onChange={handlePassword}/> 
-      </div>{errorMsg&&<div style={{fontSize: "0.8", color:"red"}}>{errorMsg}</div>}
-      <button onClick={submitLogin}>Login</button>
-    </form>)}
-      </>
+        (<>
 
-    
+
+
+          {isloggedIn ? (
+            <>
+
+              <h1 style={{ color: "blue" }}>Login success! Hello{user.name}</h1> :
+              <div style={{ border: "solid black 2px" }}>
+                <h2>Role:{user.role}</h2>
+                <h4>Employee code:{user.employeeCode}</h4>
+              </div>
+            </>
+          ) : (
+            <form>
+
+              <h1>Login</h1>
+              <div>
+                <label> Username:</label>
+                <input type="email" onChange={handleEmail} />
+              </div>
+              <div>
+                <label > Password:</label>
+                <input type="password" onChange={handlePassword} />
+              </div>{errorMsg && <div style={{ fontSize: "0.8", color: "red" }}>{errorMsg}</div>}
+              <button onClick={submitLogin}>Login</button>
+              <div>
+                Not Signed up yet?<a onClick={()=>{setPageType("signup")}}href="a">Click here to Register account</a></div>
+              
+            </form>
+
+          )}
+
+
+        </>
+        ) : (
+          <div>
+            <h1>signup</h1>
+            <form>
+
+              <div>
+                <label>First Name:</label>
+                <input type="text" onChange={handleEmail} />
+              </div>
+
+              <div>
+                <label>Last Name:</label>
+                <input type="text" onChange={handleEmail} />
+              </div>
+
+              <div>
+                <label>Email:</label>
+                <input type="email" onChange={handleEmail}></input>
+              </div>
+
+              <div>
+                <label>Password:</label>
+                <input type="Password" onChange={handleEmail}></input>
+              </div>
+
+
+              <div>
+                <label>Confirm Password:</label>
+                <input type="Password" style={{border :`1px ${samePassword? "green":"red"} solid`}} onChange={handleEmail}></input>
+              </div>
+              <button>Signup</button>
+              <div>
+                Have an account already?<a onClick={() => { setPageType("login") }} href="a">Click here to login</a>
+              </div>
+
+
+            </form>
+          </div>
+        )}
+
+    </>
+
+
 
 
 
@@ -80,5 +189,5 @@ function App() {
 
 
 
-}    
+}
 export default App
